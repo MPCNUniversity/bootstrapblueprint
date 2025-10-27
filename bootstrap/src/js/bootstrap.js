258b50f0064b481a8f53decb690a2ed545f90477
Friedel <mpcnetwork.manager@outlook.com>
2025-10-27 08:20:16 +0100

    bootstrap blueprint updated

---
+++ bootstrap/src/js/bootstrap.js
@@ -0,0 +1,4 @@
+console.log("Hallo wereld");
+console.log("MPCNetwork - bootstrap session : Activate");
+console.log("Website tutorial: bootstrap basis");
+let userName=prompt("Wat is jouw naam user?");
